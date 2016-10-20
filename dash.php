5834508b4dcfa3a126e7a3c8cfcee64e577c9fe9
Christian Påbøl Jacobsen <cpcp800@auroragames.dk>
2016-10-20 10:59:03 -0400

    prettyfying and started working on status boxes.

--- dash.php
+++ dash.php
@@ -9,10 +9,28 @@
 
 <div id="sites">
 
-
+</div>
+<div class="box">
+	<div class="boxHead">Battery</div>
+	<div class="boxBody">
+		<p id="battery"><?php system("acpi") ?> </p>
+	</div>
+</div>
+<div class="box">
+	<div class="boxHead">System Status</div>
+	<div class="boxBody">Sample text <br/> More samples<br/> Lorem ipsum dolor sit amet<br/>I really should sleep</div>
+</div>
+<div class="box">
+	<div class="boxHead">System Status</div>
+	<div class="boxBody">Sample text <br/> More samples<br/> Lorem ipsum dolor sit amet<br/>I really should sleep</div>
+</div>
+<div class="box">
+	<div class="boxHead">System Status</div>
+	<div class="boxBody">Sample text <br/> More samples<br/> Lorem ipsum dolor sit amet<br/>I really should sleep</div>
 </div>
 
 <script src="jquery.js"></script>
 <script src="config.js"></script>
+<script src="format.js"></script>
 </body>
 </html>
